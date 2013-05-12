dd3fc11f3da87c09b2370d9e101d03a363c1536c
wuqc <wuqc@wuqc-ubuntu>
2013-05-12 09:52:19 +0800

    add autoloader exception

--- libs/Mvc/Autoloader.php
+++ libs/Mvc/Autoloader.php
@@ -3,44 +3,65 @@
 /**
  * Autoloader 自动 加载 类，主要为了防止与公共 类中的__autoload()函数冲突
  * 且对目录层级没有限制
- * 
+ *
  * @author wuqingcheng
  * @create by 2013.04.03
  */
 
-class Mvc_Autoloader 
+class Mvc_Autoloader
 {
     private $_path;
-    
-    public function register() 
+
+    public function register()
     {
         spl_autoload_register(array($this, "load"));
     }
-    
+
     /*
      * 根据命名空间加载相应的include path
      */
-    private function load($className) 
+    private function load($className)
     {
         if (class_exists($className, false) || interface_exists($className, false)) {
             return false;
         }
-        
+
         //命名空间列表
         $pathArray = array(
-            'www' => "./controller/Www",
-            'mvc' => "./libs/Mvc",
-            'dang' => "./libs/Dang",
+            'web' => "./controller",
+            'cli' => "./controller",
+            'mvc' => "./libs",
+            'dang' => "./libs",
+            'zend' => "./libs",
+            'quick' => "./libs",
+            'model' => "./libs",
         );
-        
-        preg_match("/^([a-z]+)_/si", $className, $m);
+
+        preg_match("/^[\\\]?([a-z]+)([_\\\])/si", $className, $m);
         if(!$m){
-            exit($className." has no namespace");
+            throw new Exception($className." no namespace!");
         }
         $namespace = $m[1];
+        $separator = $m[2];
+
+        if(!isset($pathArray[strtolower($namespace)])){
+            throw new Exception("class: ".$className." not found!");
+        }
         $path = $pathArray[strtolower($namespace)];
-        
-        $filename = realpath($path)."/".str_replace('_', '/', preg_replace("/^([a-z]+)_/si", "", $className)) . '.php';
+
+        //捕获zend
+        if(strtolower($namespace) == "zend"){
+            if($separator == "_"){
+                $path = "./library";
+            }else{
+                $path = "./vendor/ZF2/library";
+            }
+        }
+
+        $filename = realpath($path)."/". preg_replace('/[_\\\]/', DIRECTORY_SEPARATOR, $className) . '.php';
+        if(!file_exists($filename)){
+            throw new Exception("File: ".$filename." not found!");
+        }
 
         return include $filename;
     }
