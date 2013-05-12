6b0a63e88828037917a72cd13ac9035c57165967
wuqc <wuqc@wuqc-ubuntu>
2013-05-12 09:54:53 +0800

    change public/index.php error_reporting

--- public/index.php
+++ public/index.php
@@ -5,14 +5,14 @@
  */
 
 chdir(dirname(dirname(__FILE__)));
-        
-error_reporting("E_ALL");
+
+error_reporting("2047");
 
 include "./libs/Mvc/Autoloader.php";
 //设置自动加载
 $autoloader = new Mvc_Autoloader();
 $autoloader->register();
-        
+
 //初始化入口
 $enter = new Mvc_Enter();
 //执行控制器里的代码
