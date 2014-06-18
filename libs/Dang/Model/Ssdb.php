8d41f33caad4d72b50468471f1544ff1f95da654
wuqingcheng <wqc200@gmail.com>
2014-06-18 16:52:34 +0800

    增加 ssdb 的 model

---
+++ libs/Dang/Model/Ssdb.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Dang\Model;
+
+abstract class Ssdb
+{
+    protected static $_instance = array();
+
+    public function __construct()
+    {
+    }
+
+    public static function instance()
+    {
+        $_className = get_called_class();
+        if (!isset(self::$_instance[$_className])) {
+            self::$_instance[$_className] = new $_className();
+        }
+
+        return self::$_instance[$_className];
+    }
+}
