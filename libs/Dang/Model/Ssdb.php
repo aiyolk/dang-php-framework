1100e84b33e35a7d004976406cc60c5d77460680
wuqingcheng <wqc200@gmail.com>
2014-12-01 15:16:02 +0800

    增加batch方法

--- libs/Dang/Model/Ssdb.php
+++ libs/Dang/Model/Ssdb.php
@@ -5,19 +5,31 @@ namespace Dang\Model;
 abstract class Ssdb
 {
 	protected static $_instance = array();
-
-    public function __construct()
-    {
+	
+    function __construct(){
     	
     }
 
-    public static function instance()
-    {
+    public static function instance(){
         $_className = get_called_class();
         if (!isset(self::$_instance[$_className])) {
             self::$_instance[$_className] = new $_className();
         }
 
         return self::$_instance[$_className];
+    }
+    
+    /**
+     * 批量操作开始
+     */
+    public function batchStart(){
+        $this->_ssdb->batch();
+    }
+    
+    /**
+     * 批量操作结束
+     */
+    public function batchEnd(){
+        $this->_ssdb->exec();
     }
 }
