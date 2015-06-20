3f30dbe41f730e25db5b4658203e48f6ac652904
wuqingcheng <wqc200@gmail.com>
2015-06-20 18:00:46 +0800

    PDO实例设置错误模式

--- libs/Dang/Sql/MysqlPdo.php
+++ libs/Dang/Sql/MysqlPdo.php
@@ -17,6 +17,7 @@ class MysqlPdo
                 \PDO::ATTR_PERSISTENT => true,
                 \PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'",
             ));
+            $db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
         } catch (PDOException $e) {
             echo 'Connection failed: ' . $e->getMessage();
         }
