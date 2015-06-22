d9f3de8374dfae9dea7b11c2f6d7786520218665
wuqingcheng <wqc200@gmail.com>
2015-06-22 22:16:05 +0800

    增加footerScript视图助手

--- libs/Dang/Mvc/View/HelperManager.php
+++ libs/Dang/Mvc/View/HelperManager.php
@@ -7,6 +7,7 @@ class Dang_Mvc_View_HelperManager
         'url'                 => 'Dang_Mvc_View_Helper_Url',
         'headMeta'            => 'Dang_Mvc_View_Helper_HeadMeta',
         'headScript'          => 'Dang_Mvc_View_Helper_HeadScript',
+        'footerScript'          => 'Dang_Mvc_View_Helper_FooterScript',
         'headTitle'           => 'Dang_Mvc_View_Helper_HeadTitle',
         'inlineScript'        => 'Dang_Mvc_View_Helper_InlineScript',
         'layout'              => 'Dang_Mvc_View_Helper_Layout',
